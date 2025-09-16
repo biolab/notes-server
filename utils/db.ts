ae57ecb0ce54a7ee1ea38458cd4a036aff1df23d
janezd <janez.demsar@fri.uni-lj.si>
2025-09-16 20:08:18 +0200

    Gh workflow: Add a dummy NEXT_NOTES_PATH

--- utils/db.ts
+++ utils/db.ts
@@ -1,15 +1,21 @@
 import sqlite3 from "sqlite3";
 import path from "node:path";
-import { open } from "sqlite";
+import { open, Database } from "sqlite";
 
+let db: Database;
 
-const DB_PATH = path.join(process.cwd(), "db");
-const DB_FILE = path.join(DB_PATH, "notes.sqlite");
+if (!process.env.CI) {
+  const DB_PATH = path.join(process.cwd(), "db");
+  const DB_FILE = path.join(DB_PATH, "notes.sqlite");
 
-const db = await open({
-  filename: path.join(DB_FILE),
-  driver: sqlite3.Database,
-});
-await db.exec("PRAGMA foreign_keys = ON");
+  db = await open({
+    filename: DB_FILE,
+    driver: sqlite3.Database,
+  });
+
+  await db.exec("PRAGMA foreign_keys = ON");
+} else {
+  db = {} as unknown as Database;
+}
 
 export default db;
