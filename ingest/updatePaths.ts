9286304816757db652b23015ec94584ecc862afa
janezd <janez.demsar@fri.uni-lj.si>
2026-06-15 15:19:21 +0200

    Add inheritables to updateRoot

--- ingest/updatePaths.ts
+++ ingest/updatePaths.ts
@@ -9,7 +9,7 @@ import { parseBook, RawBookDef, RawChapterDef } from "./book";
 import { gatherRedirections, updateRedirections } from "./redirections";
 import { catchErrors, hasError, logError, logWarning, printWarnings, resetError } from "./errors";
 import { MailPath } from "@/ingest/mail";
-import { InheritableResources } from "@/ingest/inheritables";
+import { InheritableResources, inheritableResourcesFromPath } from "@/ingest/inheritables";
 import {joinedPath} from "@/ingest/paths";
 import {load} from "js-yaml";
 import { UnlockChaptersOnAnswersOptions } from "@/types";
@@ -640,6 +640,12 @@ const cleanup = async (
   );
 }
 
+const getNewBuildId = async (db: Database, pathPrefix: string): Promise<number> =>
+  (await db.get(
+    `INSERT INTO builds (path) VALUES (?) RETURNING id`,
+    [pathPrefix || ""]
+  )).id;
+
 export const updatePaths = async (
   bookSlugs: string[][],
   collectionSlugs: string[][],
@@ -683,10 +689,7 @@ export const updatePaths = async (
   await db.exec("BEGIN TRANSACTION");
   let buildId: number;
   if (buildId_ === "new") {
-    buildId = (await db.get(
-      `INSERT INTO builds (path) VALUES (?) RETURNING id`,
-      [pathPrefix || ""]
-    )).id;
+    buildId = await getNewBuildId(db, pathPrefix);
   }
   else {
     await db.run(
@@ -714,15 +717,15 @@ export const updatePaths = async (
 };
 
 export const updateRoot = async (db: Database) => {
+  const buildId = await getNewBuildId(db, "");
+
   const rootCollection = await parseCollection([], []);
   if (rootCollection.frontmatter.public) {
-    const buildId = (await db.get(
-      `INSERT INTO builds (path) VALUES (?) RETURNING id`,
-      [""])
-    ).id;
     await insertCollections([rootCollection], db, buildId);
   }
-  else {
-    await db.run(`DELETE FROM collections WHERE path = ''`);
-  }
+
+  const resources = inheritableResourcesFromPath("");
+  await insertResourcePaths(resources, db, await buildId);
+
+  await cleanup(db, "", buildId);
 }
